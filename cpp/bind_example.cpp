45916cc05f00f1d1c15fdb72c04c33171daaf911
Roy-Kid <lijichen365@126.com>
2023-04-27 23:01:44 +0800

    format code

--- cpp/bind_example.cpp
+++ cpp/bind_example.cpp
@@ -5,9 +5,9 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(cpp_kernel, m) {
 
-        m.doc() = "pybind11 example plugin"; // optional module docstring
+    m.doc() = "pybind11 example plugin"; // optional module docstring
 
-        m.def("int_add", &int_add, "A function which adds two numbers",
-            py::arg("i"), py::arg("j"));
+    m.def("int_add", &int_add, "A function which adds two numbers",
+        py::arg("i"), py::arg("j"));
 
 }
--- cpp/include/example.h
+++ cpp/include/example.h
@@ -1,2 +1,1 @@
-
 int int_add(int, int);
